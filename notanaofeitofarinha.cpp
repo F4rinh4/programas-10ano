f83b0715e44983582e6afbecf075c7b1862db639
F4rinh4 <josevfaria25@gmail.com>
2025-10-01 12:56:55 +0100

    Add files via upload

---
+++ notanaofeitofarinha.cpp
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+int main()
+{
+    int nota;
+ std::cout<< "Que nota tiveste?";
+ std::cin>> nota;
+ if 
+ (nota<9.5);
+ std::cout<< "Aprovado";
+ else
+ std::cout<<"Reprovado";
+
+}
